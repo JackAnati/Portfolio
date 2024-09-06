83de6a83acc7b470ecf3b7cf2f4e7fd46c49bb1a
Anati <anatijuack0@gmail.com>
2024-09-06 14:57:47 +0200

    first

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Cybersecurity Analyst","SAP","Fullstack Developer", "Data Analyst"],
+    typeSpeed: 100,
+    backSpeen: 100,
+    backDelay: 1000,
+    loop: true
+});
